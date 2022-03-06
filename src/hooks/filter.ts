36c37c0e17d3f1fdf2456875d0a6b09c45764149
Viviana Davila <vdavila@ucsd.edu>
2022-03-05 17:47:58 -0800

    more ts fixes

--- src/hooks/filter.ts
+++ src/hooks/filter.ts
@@ -1,22 +1,25 @@
-export default function countriesFilter(arr, name, region) {
-  
+export default function countriesFilter(
+  arr: any[],
+  name: string,
+  region: string
+) {
   const countryFilter = arr.filter((item) => {
     if (item.name.official.toLowerCase().includes(name.toLowerCase().trim())) {
-      return item
+      return item;
     }
 
-    return item.name.official.toLowerCase() === name.toLowerCase().trim()
+    return item.name.official.toLowerCase() === name.toLowerCase().trim();
   });
 
-  let resultFilter = countryFilter
+  let resultFilter = countryFilter;
 
   if (region.length > 1) {
-    if (region === 'All') resultFilter = countryFilter
+    if (region === "All") resultFilter = countryFilter;
     else
       resultFilter = countryFilter.filter((item) => {
-        return item.region.toLowerCase() === region.toLowerCase().trim()
-      })
+        return item.region.toLowerCase() === region.toLowerCase().trim();
+      });
   }
 
-  return resultFilter
+  return resultFilter;
 }
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -6,18 +6,6 @@ interface Response {
   loading: boolean;
 }
 
-// everytime you type in the search field, this gets re-rendered
-// that's fine, what I don't want recalculated everytime is the filter function
-// wait no i do want that recalculated when region and search change
-// but i want the calculation saved if ive done it before
-// if the region is africa, then i change it to europe, then go back to africa,
-// i dont want the africa calculation done all over again.
-// if usememo doesnt apply i can try normal memoization
-// im assuming the filter function to be a really slow calculation
-
-// maybe i can use usecallback here so usefetch isnt called whenever i type? not sure
-// i can focus on this later
-
 const useFetch = (url: string) => {
   const [resp, setResp] = useState<Response>({ data: [], loading: true });
 
